a9b96b51e742fc6c81b99b10329a2edc8c6a6634
mutantsan <kvaqich@gmail.com>
2024-03-13 14:43:40 +0200

    wip: implementing chats

--- ckanext/chatbot/views.py
+++ ckanext/chatbot/views.py
@@ -1,18 +1,21 @@
 from __future__ import annotations
 
 import logging
-
 import os
 
+import requests
 from openai import OpenAI
 from flask import Blueprint, Response
 from flask.views import MethodView
 
 import ckan.plugins.toolkit as tk
 
+from ckanext.chatbot import utils
+
 
 log = logging.getLogger(__name__)
 bp = Blueprint("chatbot", __name__)
+bp.before_request(utils.before_request)
 
 client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
 
@@ -20,17 +23,18 @@ client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
 class ChatBotTalkView(MethodView):
 
     def get(self) -> str:
-        return tk.render("chatbot/talk.html", extra_vars={"data": {}, "errors": {}})
+        return tk.render(
+            "chatbot/talk.html",
+            extra_vars={"chats": utils.get_user_chats(tk.current_user.id)},  # type: ignore
+        )
 
     def post(self) -> str:
-        promt = tk.request.form["promt"]
+        prompt = tk.request.form["prompt"]
 
-        if promt:
-            response = self.generate_response(promt)
+        if prompt:
+            response = self.generate_response(prompt)
         else:
-            response = "Please, provide a promt"
-
-        print(response)
+            response = "Please, provide a prompt"
 
         return response
 
@@ -39,7 +43,23 @@ class ChatBotTalkView(MethodView):
             messages=[
                 {
                     "role": "system",
-                    "content": "Marv is a factual chatbot that is also sarcastic",
+                    "content": "Marv is a CKAN AI assistant bot and he answers only questions related to CKAN platform",
+                },
+                {
+                    "role": "system",
+                    "content": "When someone asks Marv about questions not related to CKAN, he says that he can help only with CKAN",
+                },
+                {
+                    "role": "system",
+                    "content": "Marv understands, that he's a CKAN AI assistant and he is on a CKAN portal",
+                },
+                {
+                    "role": "system",
+                    "content": "Marv understands, that user that asks a question is logged in to a CKAN portal and doesn't suggest them to log in",
+                },
+                {
+                    "role": "system",
+                    "content": "Marv returns a response in markdown format",
                 },
                 {
                     "role": "user",
@@ -47,11 +67,23 @@ class ChatBotTalkView(MethodView):
                 },
             ],
             model="ft:gpt-3.5-turbo-0125:personal::91xHVRq2",
-            max_tokens=100,
+            # max_tokens=100,
             n=1,
         )
 
         return response.choices[0].message.content or ""
 
 
+class ChatBotChatView(MethodView):
+    def get(self, chat_id: str) -> str:
+        return tk.render(
+            "chatbot/talk.html",
+            extra_vars={
+                "chats": utils.get_user_chats(tk.current_user.id),  # type: ignore
+                "active_chat": utils.get_chat(tk.current_user.id, chat_id),  # type: ignore
+            },
+        )
+
+
 bp.add_url_rule("/chatbot/talk", view_func=ChatBotTalkView.as_view("talk"))
+bp.add_url_rule("/chatbot/talk/<chat_id>", view_func=ChatBotChatView.as_view("chat"))
